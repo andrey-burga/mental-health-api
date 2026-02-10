4d3a2b6f5cfb31d5568d09c6d5402bacf8f5d03c
Andrey B. <jhonyandreyburga@gmail.com>
2026-02-10 16:07:38 -0500

    Initial backend setup production

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -1,17 +1,5 @@
 // src/config/prisma.ts
-import "dotenv/config"; // carga .env
+import "dotenv/config";
 import { PrismaClient } from "@prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
-import pkg from "pg";
 
-const { Pool } = pkg;
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL, // usa tu .env
-});
-
-const adapter = new PrismaPg(pool);
-
-export const prisma = new PrismaClient({
-  adapter,
-});
+export const prisma = new PrismaClient();
